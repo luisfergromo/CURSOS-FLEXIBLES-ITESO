e428cd8d0b03306305cd4f0809a57f1924a1986f
Luis Romo <changoluis@hotmail.com>
2024-01-22 11:50:52 -0600

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,15 @@
+$('body').append("<div class='container-fluid bg-amarillo cintillo'>" +
+  "<div class='row'>" +
+    "<div class='container'>" +
+      "<div class='row'>" +
+        "<div class='col-md-6'><img src='https://test.adview.mx/canvasiteso/images/logo_iteso.png' alt='' class='img-fluid'></div>" +
+        "<div class='col-md-6'>" +
+          "<ul>" +
+            "<li><a href='#'>NOTICIAS</a></li>" +
+            "<li><a href='#'>EVENTOS</a></li>" +
+          "</ul>" +
+        "</div>" +
+      "</div>" +
+    "</div>" +
+  "</div>" +
+"</div>");
