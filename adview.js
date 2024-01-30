503b4ad74c79002955e7010f051897ab9978107e
Luis Romo <changoluis@hotmail.com>
2024-01-30 13:16:17 -0600

    Create adview.js

---
+++ adview.js
@@ -0,0 +1,89 @@
+$('body').append("<div class='container-fluid bg-amarillo cintillo'>" +
+  "<div class='row'>" +
+    "<div class='container'>" +
+      "<div class='row'>" +
+        "<div class='col-md-6'><img src='https://test.adview.mx/canvasiteso/images/logo_iteso.png' alt='' class='img-fluid'></div>" +
+        "<div class='col-md-6'>" +
+          "<ul>" +
+            "<li><a href='/?query=en%20linea'></a></li>" +
+            "<li><a href='#'></a></li>" +
+          "</ul>" +
+        "</div>" +
+      "</div>" +
+    "</div>" +
+  "</div>" +
+"</div>");
+
+$('#app-footer').append(
+  "<div class='container-fluid bg-azul-footer'>" +
+    "<div class='container'>" +
+      "<div class='row'>" +
+        "<div class='col-md-12 contenido-principal'>" +
+          "<h2 class='txt-blanco'>Instituto Tecnológico y de Estudios Superiores de Occidente</h2>" +
+          "<h3 class='txt-blanco'>Periférico Sur Manuel Gómez Morín # 8585 C.P. 45604 Tlaquepaque Jalisco, México</h3>" +
+          "<a class='tel-footer' href='tel:523336693434'> +52 33 3669 3434</a>" +
+        "</div>" +
+        "<div class='col-md-12 redes-sociales'>" +
+          "<div class='redes'> " +
+            "<a href='https://www.facebook.com/ITESO/' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fab fa-facebook-f fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>Facebook</p>" +
+            "</a>" +
+            "<a href='https://twitter.com/ITESO' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fa-brands fa-x-twitter fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>Twitter</p>" +
+            "</a>" +
+            "<a href='https://www.instagram.com/itesouniversidad/' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fab fa-instagram fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>Instagram</p>" +
+            "</a>" +
+            "<a href='https://www.youtube.com/c/ITESOuniversidad' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fab fa-youtube fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>Youtube</p>" +
+            "</a>" +
+            "<a href='https://www.tiktok.com/@itesouniversidad' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fab fa-tiktok fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>TikTok</p>" +
+            "</a>" +
+            "<a href='https://www.linkedin.com/school/iteso-universidad/' class='me-4 text-reset btn-outline-light btn-floating'>" +
+              "<div class='social'><i class='fab fa-linkedin-in fa-3x' style='color: #00c4ff;'></i></div>" +
+              "<p>LinkedIn</p>" +
+            "</a>" +
+          "</div>" +
+        "</div>" +
+        "<div class='col-md-12 logos-footer'>" +
+          "<ul class=''>" +
+            "<li class='seleccionado'>" +
+              "<a target='_blank' href='http://educacionjesuita.com.mx/'>" +
+                "<img width='125px' alt='' data-fileentryid='20153' src='https://iteso.mx/documents/123/20150/logo-suj.svg/f16a1f50-12e5-9f33-b23f-adcc5613f317?t=1635968264178'>" +
+              "</a>" +
+            "</li>" +
+            "<li class='seleccionado'>" +
+              "<a target='_blank' href='https://www.ausjal.org/'>" +
+                "<img width='125px' alt='' data-fileentryid='20157' src='https://iteso.mx/documents/123/20150/logo-ausjal.svg/e4e435f0-60c1-9cd2-fb7d-338be3ef6c54?t=1635968264178'>" +
+              "</a>" +
+            "</li>" +
+            "<li class='seleccionado'>" +
+              "<a target='_blank' href='http://www.anuies.mx/'>" +
+                "<img width='125px' alt='' data-fileentryid='20161' src='https://iteso.mx/documents/123/20150/logo-anuies.svg/b534b3ec-f107-cedc-634b-0740757834c3?t=1635968264178'>" +
+              "</a>" +
+            "</li>" +
+            "<li class='seleccionado'>" +
+              "<a target='_blank' href='https://www.fimpes.org.mx/'>" +
+                "<img width='125px' alt='' data-fileentryid='20165' src='https://iteso.mx/documents/123/20150/logo-fimpes.svg/21fc3f60-4825-a7b9-7e42-1d991d69ba8f?t=1635968264178'>" +
+              "</a>" +
+            "</li>" +
+            "<li class='seleccionado'>" +
+              "<a target='_blank' href=''>" +
+                "<img width='125px' alt='' data-fileentryid='20169' src='https://iteso.mx/documents/123/20150/logo-excelencia-academica.svg/14b0cc00-a7a8-aafa-7dc3-d2c5b993913c?t=1635968264178'>" +
+              "</a>" +
+            "</li>" +
+          "</ul>" +
+        "</div>" +
+        "<div class='col-md-12'>" +
+          "<p class='txt-blanco'>Aviso de privacidad ┃ Información de contacto y soporte</p>" +
+        "</div>" +
+      "</div>" +
+    "</div>" +
+  "</div>"
+);
